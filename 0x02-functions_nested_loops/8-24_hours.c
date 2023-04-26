32a94ba02c0e1cc306ebb8aef832210a80cbe491
NmaGold <echefugoldnma@gmail.com>
2023-04-26 02:03:03 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer
+ * jack_bauer - function that prints every minute
+ * of the day of Jack Bauer
  * starting from 00:00 to 23:59
  */
 void jack_bauer(void)
@@ -19,7 +20,7 @@ void jack_bauer(void)
 			_putchar((j / 10) + '0');
 			_putchar((j % 10) + '0');
 			_putchar('\n');
-			j++
+			j++;
 		}
 		i++;
 	}
