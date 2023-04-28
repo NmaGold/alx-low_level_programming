a3a8e989465b3a22b75b007a87d5d32ac6555f28
NmaGold <echefugoldnma@gmail.com>
2023-04-28 06:52:38 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,10 +5,11 @@
  * @c: char to be checked
  * Return:1 if uppercase, 0 if otherwise
  */
+
 int _isupper(int c)
 {
-	(if  c >= 'A' && c <= 'Z')
-		return (1)
-		else
-			return (0)
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0)
 }
